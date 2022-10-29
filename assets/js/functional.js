1d0d20124fde47fee3ca719bc4fc372ec584a25e
Maria2721 <84140203+Maria2721@users.noreply.github.com>
2022-10-29 18:19:21 +0300

    merge conflict

--- assets/js/functional.js
+++ assets/js/functional.js
@@ -1,5 +1,29 @@
-function openCard() {
-  location.href = "card_template.html";
+function openCardFilms() {
+  location.href = "watch_films.html";
+}
+
+function openCardSnowman() {
+  location.href = "snowman.html";
+}
+
+function openCardSanta() {
+  location.href = "secret-santa.html";
+}
+
+function openCardResults() {
+  location.href = "results_of_year.html";
+}
+
+function openCardFairs() {
+  location.href = "christmas-fairs.html";
+}
+
+function openCardBathtime() {
+  location.href = "bath-time.html";
+}
+
+function openCardFiveMin() {
+  location.href = "5_min.html";
 }
 
 function closeCard() {
